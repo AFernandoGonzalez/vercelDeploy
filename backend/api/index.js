676274eb2bd84c67b0aabb7205767327c86826dc
Fernando <ferg.developer@gmail.com>
2023-12-10 10:25:07 -0500

    vercel.json added

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,7 @@ const setupApp = require('../app');
 
 const app = setupApp();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
